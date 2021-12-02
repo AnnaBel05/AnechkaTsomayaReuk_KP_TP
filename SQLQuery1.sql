2ebaac78a312c89c2959ce24b7b4bc6073af47b2
AnnaBel05 <anebuchenkova@yandex.ru>
2021-12-02 18:19:10 +0300

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿drop table students;
+CREATE TABLE students
+(
+    Id int identity,
+    Last_name varchar(256) NOT NULL,
+    First_name varchar(256) NOT NULL,
+    Middle_name varchar(256) NOT NULL,
+    Birth_date datetime NOT NULL,
+    Phone_number varchar(256) NOT NULL,
+    Credit_card_number varchar(256) NOT NULL,
+    
+    Educational_form int NOT NULL,
+    Basis_of_training int NOT NULL
+);
